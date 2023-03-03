95779dcd641c44fda687b8e9c8d275583a14d29e
sfernandez <sfdezlop@gmail.com>
2023-03-03 07:17:33 +0100

    Add auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import { config } from '../config.js';
+import { HTTPError } from '../interfaces/error.js';
+
+export interface PayloadToken extends jwt.JwtPayload {
+  id: string;
+  email: string;
+  role: string;
+}
+
+const salt = 10;
+
+export class Auth {
+  static createJWT(payload: PayloadToken) {
+    return jwt.sign(payload, config.jwtSecret as string);
+  }
+
+  static verifyJWTGettingPayload(token: string) {
+    const result = jwt.verify(token, config.jwtSecret as string);
+    if (typeof result === 'string')
+      throw new HTTPError(498, 'Invalid payload', result);
+    return result as PayloadToken;
+  }
+
+  static hash(value: string) {
+    return bcrypt.hash(value, salt);
+  }
+
+  static compare(value: string, hash: string) {
+    return bcrypt.compare(value, hash);
+  }
+}
